9f271085d3957a98537228479416fccb5eb7c596
Daniel Kaczorowski <kaczorowski.daniel@gmail.com>
2015-11-23 11:32:37 +0100

    Start

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Bêdzie to program, który obliczy funkcje liniow¹/kwadratow¹/szeœcienn¹.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Start programu.");
+    }
+}
