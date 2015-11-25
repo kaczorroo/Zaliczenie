9d3051e107465f6da4aa25c315ea57088d40dedc
Daniel Kaczorowski <kaczorowski.daniel@gmail.com>
2015-11-25 11:05:37 +0100

    liniowa i kwadratowa gotowa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * Będzie to program, który obliczy funkcje liniową/kwadratową/sześcienną.
  */
@@ -8,5 +10,77 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Start programu.");
+        System.out.println("Podaj, który rodzaj funkcji chcesz obliczyć. (Poprzez wpisanie a,b lub c.)");
+        System.out.println("1)Liniowa");
+        System.out.println("2)Kwadratowa");
+        System.out.println("3)Sześcienna");
+        double f;
+        do {
+            Scanner funkcja = new Scanner(System.in);
+            f = funkcja.nextInt();
+            if (f != 1 && f != 2 && f != 3) {
+                System.out.println("Wybierz jedną z trzech możliwych opcji.");
+            }
+        }
+        while (f==1 && f==2 && f==3);
+            if(f==1){
+                System.out.println("Wybrałeś funkcję Liniową.");
+                System.out.println("f(x)=ax+b");
+                System.out.println("Proszę podać a.");
+                double a, b, wynik;
+                Scanner na = new Scanner(System.in);
+                a = na.nextInt();
+                System.out.println("Proszę podać b.");
+                Scanner nb = new Scanner(System.in);
+                b = nb.nextInt();
+                wynik = -(b/a);
+                if(a==0){
+                    System.out.println("Funkcja liniowa jest stała.");
+                    System.out.println("Nie posiada miejsc zerowych");
+                }
+                else if(a>0){
+                    System.out.println("Funkcja liniowa jest rosnąca.");
+                    System.out.println("A miejsce zerowe wynosi " + wynik);
+                }
+                else if(a<0){
+                    System.out.println("Funkcja liniowa jest malejąca");
+                    System.out.println("A miejsce zerowe wynosi " + wynik);
+                }
+            }
+            if (f==2){
+                System.out.println("Wybrałeś funkcję kwadratową.");
+                System.out.println("f(x)=ax2+bx+c");
+                int ak, bk, ck;
+                System.out.println("Proszę podać a.");
+                Scanner akk = new Scanner(System.in);
+                ak = akk.nextInt();
+                System.out.println("Proszę podać b.");
+                Scanner bkk = new Scanner(System.in);
+                bk = bkk.nextInt();
+                System.out.println("Proszę podać c.");
+                Scanner ckk = new Scanner(System.in);
+                ck = ckk.nextInt();
+                double deltak;
+                double delta = bk * bk - 4 * ak *ck;
+                deltak = Math.sqrt(delta);
+                double x1 = (-bk - deltak)/2*ak;
+                double x2 = (-bk + deltak)/2*ak;
+                double x0 = -bk/2*ak;
+                if(delta>0){
+                    System.out.println("x1= "+x1);
+                    System.out.println("x2= "+x2);
+                }
+                else if(delta==0){
+                    System.out.println("x0 = "+x0);
+                }
+                else if(delta<0){
+                    System.out.println("Nie ma rozwiązania.");
+                }
+
+            }
+            if(f==3){
+                System.out.println("Wybrałeś funkcję sześcienną.");
+                System.out.println("f(x) = ax3 + bx2 + cx + d");
+            }
     }
 }
