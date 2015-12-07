3cd721d098599ad485f44fdd365b64f5d669d058
Daniel Kaczorowski <kaczorowski.daniel@gmail.com>
2015-12-07 13:25:27 +0100

    poprawka ustawienia

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 /**
- * Będzie to program, który obliczy funkcje liniową/kwadratową/sześcienną.
+ * Temat: Rozwiązywanie równań pierwszego, drugiego i trzeciego stopnia. Implementacja  w języku Java.
  */
 
 public class Main {
@@ -18,7 +18,7 @@ public class Main {
         do {
             Scanner funkcja = new Scanner(System.in);
             f = funkcja.nextInt();
-            if (f != 1 && f != 2 && f != 3) {
+            if (f!=1 && f!=2 && f!=3) {
                 System.out.println("Wybierz jedną z trzech możliwych opcji.");
             }
         }
@@ -27,39 +27,39 @@ public class Main {
                 System.out.println("Wybrałeś funkcję Liniową.");
                 System.out.println("f(x)=ax+b");
                 System.out.println("Proszę podać a.");
-                double a, b, wynik;
-                Scanner na = new Scanner(System.in);
-                a = na.nextInt();
+                    double a1, b1, wynik1;
+                        Scanner na = new Scanner(System.in);
+                        a1 = na.nextInt();
                 System.out.println("Proszę podać b.");
-                Scanner nb = new Scanner(System.in);
-                b = nb.nextInt();
-                wynik = -(b/a);
-                if(a==0){
+                    Scanner nb = new Scanner(System.in);
+                    b1 = nb.nextInt();
+                    wynik1 = -(b1/a1);
+                if(a1==0){
                     System.out.println("Funkcja liniowa jest stała.");
                     System.out.println("Nie posiada miejsc zerowych");
                 }
-                else if(a>0){
+                else if(a1>0){
                     System.out.println("Funkcja liniowa jest rosnąca.");
-                    System.out.println("A miejsce zerowe wynosi " + wynik);
+                    System.out.println("A miejsce zerowe wynosi " + wynik1);
                 }
-                else if(a<0){
+                else if(a1<0){
                     System.out.println("Funkcja liniowa jest malejąca");
-                    System.out.println("A miejsce zerowe wynosi " + wynik);
+                    System.out.println("A miejsce zerowe wynosi " + wynik1);
                 }
             }
             if (f==2){
                 System.out.println("Wybrałeś funkcję kwadratową.");
                 System.out.println("f(x)=ax2+bx+c");
-                int ak, bk, ck;
+                    double ak, bk, ck;
                 System.out.println("Proszę podać a.");
-                Scanner akk = new Scanner(System.in);
-                ak = akk.nextInt();
+                    Scanner akk = new Scanner(System.in);
+                    ak = akk.nextInt();
                 System.out.println("Proszę podać b.");
-                Scanner bkk = new Scanner(System.in);
-                bk = bkk.nextInt();
+                    Scanner bkk = new Scanner(System.in);
+                    bk = bkk.nextInt();
                 System.out.println("Proszę podać c.");
-                Scanner ckk = new Scanner(System.in);
-                ck = ckk.nextInt();
+                    Scanner ckk = new Scanner(System.in);
+                    ck = ckk.nextInt();
                 double deltak;
                 double delta = bk * bk - 4 * ak *ck;
                 deltak = Math.sqrt(delta);
@@ -81,21 +81,22 @@ public class Main {
             if(f==3){
                 System.out.println("Wybrałeś funkcję sześcienną.");
                 System.out.println("f(x) = ax3 + bx2 + cx + d");
-                int as, bs, cs, ds;
+                    double a, b, c, d, p, q, x1, x2, x3;
                 System.out.println("Proszę podać wartość a.");
-                Scanner ass = new Scanner(System.in);
-                as = ass.nextInt();
+                    Scanner ass = new Scanner(System.in);
+                    a = ass.nextInt();
                 System.out.println("Proszę podać wartość b.");
-                Scanner bss = new Scanner(System.in);
-                bs = bss.nextInt();
+                    Scanner bss = new Scanner(System.in);
+                    b = bss.nextInt();
                 System.out.println("Proszę podać wartość c.");
-                Scanner css = new Scanner(System.in);
-                cs = css.nextInt();
+                    Scanner css = new Scanner(System.in);
+                    c = css.nextInt();
                 System.out.println("Proszę podać wartość d.");
-                Scanner dss = new Scanner(System.in);
-                ds = dss.nextInt();
+                    Scanner dss = new Scanner(System.in);
+                    d = dss.nextInt();
                 dss.close();
-                
+                x1 =
             }
+        System.out.println("Koniec programu.");
     }
 }
